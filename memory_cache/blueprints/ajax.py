214c4eaee43f94de63dbc0cc63db8460e710712d
kaka4NERV <ampedee@163.com>
2019-01-30 23:33:12 +0800

    add profile popup

--- memory_cache/blueprints/ajax.py
+++ memory_cache/blueprints/ajax.py
@@ -1,3 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from memory_cache.models import User
 
 ajax_bp = Blueprint('ajax', __name__)
+
+
+@ajax_bp.route('/profile/<int:user_id>')
+def get_profile(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('main/profile_popup.html', user=user)
